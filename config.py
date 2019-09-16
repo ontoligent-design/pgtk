e7e3bd51e2c825fb5af5f2cd4ed8829bece73897
Rafael Alvarado <ontoligent@gmail.com>
2019-09-16 00:33:18 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # Project Gutenberg URLs
 pg_rdf_url = "https://www.gutenberg.org/cache/epub/feeds/rdf-files.tar.zip"
-pg_index_zip = "https://www.gutenberg.org/dirs/GUTINDEX.zip"
+pg_index_url = "https://www.gutenberg.org/dirs/GUTINDEX.zip"
